1a5e3dd788d0acc43b5296537c5e21e0901695f3
Bhimaraju Abhiram <bheemarajuabhiram@gmail.com>
2026-03-20 23:33:11 +0530

    address also added

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 def get_user():
     name = input("Enter your name: ")
     age = input("Enter your age: ")
-    print(f"Name: {name}, Age: {age}")
+    address = input("Enter your address: ")
+    print(f"Name: {name}, Age: {age}, Address: {address}")
 
 get_user()
