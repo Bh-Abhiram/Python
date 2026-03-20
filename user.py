32499f80f241f08df9b697a2f25e5b815aaa5cbc
Bhimaraju Abhiram <bheemarajuabhiram@gmail.com>
2026-03-20 23:36:03 +0530

    gender also added

--- user.py
+++ user.py
@@ -2,6 +2,7 @@ def get_user():
     name = input("Enter your name: ")
     age = input("Enter your age: ")
     address = input("Enter your address: ")
-    print(f"Name: {name}, Age: {age}, Address: {address}")
+    gender = input("Enter your gender: ")
+    print(f"Name: {name}, Age: {age}, Gender: {gender}, Address: {address}")
 
 get_user()
